02f22a91ccc9c4fb2fa8d6e5a0aab16e9138294b
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-09-10 08:12:47 +0530

    Create Practice.dart

---
+++ Practice.dart
@@ -0,0 +1,13 @@
+void main(){
+  var myList = [1,2,3,4,5,6,7,8,9,10];
+  for(final item in myList){
+    if(item%2==0){
+    print(item);}
+  }
+  for(var i =0;i<myList.length;i++){
+    print(myList);
+  }
+  
+  
+  
+}
